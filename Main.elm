ac6582576779c9f5ce27ca51bca8f1c45eb86d39
Andy Aylward <aaylward@gmail.com>
2015-07-04 23:51:27 -0400

    move styles to their own module

--- Main.elm
+++ Main.elm
@@ -3,23 +3,10 @@ module Main where
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import TypedStyles exposing (..)
+
+-- project imports
 import FreeThrow
-
--- styles
-
-font : List (String, String)
-font =
-  [ ("font-family", "futura, sans-serif")
-  , ("color", "rgb(42, 42, 42)")
-  , ("font-size", "2em") ]
-
-layout : List (String, String)
-layout =
-  [ ("padding", "2em") ]
-
-background : List (String, String)
-background =
-  [ ("background-color", "rgb(255, 0, 0)") ]
+import Style exposing (font, background, layout)
 
 -- View
 app =
