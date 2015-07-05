b53e7cee8bafe18df5ad5450f0df2e52198d8493
Andy Aylward <aaylward@gmail.com>
2015-07-04 22:55:45 -0400

    weeee :us:

--- Main.elm
+++ Main.elm
@@ -1,7 +1,20 @@
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import TypedStyles exposing (..)
+
+font : List (String, String)
+font =
+  [ ("font-family", "futura, sans-serif")
+  , ("color", "rgb(42, 42, 42)")
+  , ("font-size", "2em") ]
+
+
+background : List (String, String)
+background =
+  [ ("background-color", "rgb(255, 0, 0)") ]
 
 app =
-  div [] [text "yo"]
+  div [ style (font ++ background) ] [text "yo"]
 
 main =
   app
