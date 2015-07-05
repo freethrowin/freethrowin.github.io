df3556e00e34ce2200575d18829ce3318f3c5157
Andy Aylward <aaylward@gmail.com>
2015-07-04 23:51:50 -0400

    :hamster:

---
+++ Style.elm
@@ -0,0 +1,16 @@
+module Style where
+
+font : List (String, String)
+font =
+  [ ("font-family", "futura, sans-serif")
+  , ("color", "rgb(42, 42, 42)")
+  , ("font-size", "2em") ]
+
+layout : List (String, String)
+layout =
+  [ ("padding", "2em") ]
+
+background : List (String, String)
+background =
+  [ ("background-color", "rgb(0, 255, 0)") ]
+
