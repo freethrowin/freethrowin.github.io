d25b3be9b715e10881bd0ea3193a80bf3da74fa9
~maxmechanic~ <maxmechanic@users.noreply.github.com>
2015-10-28 22:23:28 -0400

    Start building out app structure

--- Main.elm
+++ Main.elm
@@ -1,16 +1,71 @@
 module Main where
 
-import Html exposing (Html, div, text)
+import List
+import String
+import Html exposing (Html, div, text, button, input)
+import Html.Events exposing (onClick)
 import Html.Attributes exposing (style)
+import StartApp.Simple as StartApp
 
 -- project imports
-import FreeThrow
+import FreeThrow exposing (..)
 import Style exposing (font, background, layout)
 
-app : Html
-app =
-  div [ style (font ++ background ++ layout) ] [text "Freethrows are fun."]
-
-main : Html
 main =
-  app
+  StartApp.start { model = model, view = view, update = update }
+
+model : FreeThrow.Session
+model =
+  {
+    player = ""
+  , shots = []
+  , percentage = 0
+  , score = 0
+  }
+
+view address model =
+  div [ style (font ++ background ++ layout) ] [
+      text "Freethrows are fun"
+    ]
+
+takeShot : Shot -> Session -> Session
+takeShot shot session =
+  let
+    points = getShotScore shot
+    newScore = session.score + points
+    newShots = session.shots ++ [shot]
+    shotsTaken = List.length newShots
+    madeShots = getMadeShotCount newShots
+  in
+    {
+      session |
+        shots <- newShots
+        , score <- newScore
+        , percentage <- round ((toFloat madeShots / toFloat shotsTaken) * 100)
+    }
+
+getMadeShotCount : List Shot -> Int
+getMadeShotCount shots =
+  List.length (List.filter .made shots)
+
+
+getShotScore : Shot -> Int
+getShotScore {technique, made} =
+  if made then
+    case technique of
+      Layup -> 2
+      FreeThrow -> 2
+      ThreePointer -> 3
+    else 0
+
+type Action =
+  Shoot Shot
+  | ChangeShot ShotType
+  | ChangeName String
+
+update :  Action -> FreeThrow.Session -> FreeThrow.Session
+update action model =
+  case action of
+    Shoot shot ->
+      takeShot shot model
+
