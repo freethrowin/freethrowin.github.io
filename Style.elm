8ae4ad6b663bb6d4de9f0b74b6387fbdf86c66b1
~maxmechanic~ <maxmechanic@users.noreply.github.com>
2015-10-31 15:09:29 -0400

    Add some styles, change some styles style

--- Style.elm
+++ Style.elm
@@ -8,9 +8,15 @@ font =
 
 layout : List (String, String)
 layout =
-  [ ("padding", "2em") ]
+  [ ("padding", "1em")
+  , ("padding-bottom", "6em") ]
 
 background : List (String, String)
 background =
   [ ("background-color", "rgb(0, 255, 0)") ]
 
+currentSession : List (String, String)
+currentSession =
+  [ ("float", "right")
+  , ("border", "3px solid black")
+  , ("padding", "1em") ]
